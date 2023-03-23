fc8e6b7232e772d109038e0496b3186f0e92d73d
vtomogz <86956053+tbcompany-test@users.noreply.github.com>
2023-03-23 22:29:24 +0100

    added register form validation

--- src/components/base/BaseInput.vue
+++ src/components/base/BaseInput.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
-    <input :type="inputType" :placeholder="inputPlaceholder">
+  <div :class="{'invalid':!valid}">
+    <input :type="inputType" :placeholder="inputPlaceholder" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
     <span class="material-symbols-outlined">{{ inputIcon }}</span>
   </div>
-  
 </template>
 
 <script>
 export default {
-  props: ['type'],
+  props: ['type', 'modelValue', 'valid'],
+  emits: ['update:modelValue'],
   data() {
     return {
       buttonTypes: {
         email: {
-          type: 'email',
+          type: 'text',
           name: 'Adres email',
           icon: 'alternate_email'
         },
@@ -27,7 +27,7 @@ export default {
           name: 'Hasło',
           icon: 'lock'
         },
-        repeatPassword: {
+        rpassword: {
           type: 'password',
           name: 'Powtórz hasło',
           icon: 'lock'
@@ -53,6 +53,7 @@ export default {
 div {
   display: flex;
   align-content: center;
+  border-radius: 15px;
 }
 input {
   background: #FBF5F3;
@@ -62,7 +63,6 @@ input {
   color: #000000;
   border:0;
   width: 400px;
-  margin-bottom:20px;
 }
 
 input:focus {
@@ -73,8 +73,12 @@ span {
   color: rgba(0, 0, 0, 0.5);
   font-size: 40px;
   background-color: #FBF5F3;
-  margin-bottom: 20px;
   padding: 10px 15px 10px 15px;
   border-radius: 0 15px 15px 0;
+}
+
+.invalid {
+  outline: 2px solid #FF0000;
+  outline-offset: -2px;
 }
 </style>
--- src/sites/RegisterUserPage.vue
+++ src/sites/RegisterUserPage.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="content">
     <base-form>
-      <form @submit.prevent=""> 
+      <form @submit.prevent="register"> 
         <h1>Stwórz konto</h1>
-        <base-input type="email"></base-input>
-        <base-input type="username"></base-input>
-        <base-input type="password"></base-input>
-        <base-input type="repeatPassword"></base-input>
+        <base-input type="email" v-model.trim="email" :valid="isEmailValid"></base-input>
+        <base-input type="username" v-model.trim="username" :valid="isUsernameValid"></base-input>
+        <base-input type="password" v-model.trim="password" :valid="isPasswordValid"></base-input>
+        <base-input type="rpassword" v-model.trim="rpassword" :valid="isPasswordValid"></base-input>
+        <p v-if="!isFormValid">Wypełnij formularz poprawnymi danymi. Pamiętaj, że hasło musi posiadać minimum 8 znaków oraz powinno zawierać małą i dużą literę, cyfrę oraz znak specjalny.</p>
         <base-button type="green-large">Wyślij</base-button>
         <p>Jeśli posiadasz już konto możesz przejść do formularza logowania</p>
       </form>
@@ -24,6 +25,57 @@ export default {
     BaseForm,
     BaseButton,
     BaseInput
+  },
+  data() {
+    return {
+      email: '',
+      username: '',
+      password: '',
+      rpassword: '',
+      isFormValid: true,
+      isEmailValid: true,
+      isUsernameValid: true,
+      isPasswordValid: true
+    }
+  },
+  methods: {
+    validateEmail() {
+      const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      if(this.email.match(emailRegex))
+        return true;
+      this.isFormValid=false;
+      this.email='';
+      return false;
+    },
+    validateUsername() {
+      if(this.username.length>0) {
+        return true
+      }
+      this.isFormValid=false;
+      this.username='';
+      return false;
+    },
+    validatePassword() {
+      const passwordRegex = /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$/;
+      if(this.password.match(passwordRegex) && this.password == this.rpassword)
+        return true;
+      this.isFormValid=false;
+      this.password='';
+      this.rpassword='';
+      return false;
+    },
+    validateForm() {
+      this.isFormValid=true;
+      this.isEmailValid = this.validateEmail();
+      this.isUsernameValid = this.validateUsername();
+      this.isPasswordValid = this.validatePassword();
+
+    },
+    register() {
+      this.validateForm();
+      if(this.isFormValid)
+        console.log('VALID!!!');
+    }
   }
 }
 </script>
@@ -45,12 +97,13 @@ h1 {
 
 form {
   display: flex;
+  gap: 20px;
   flex-direction: column;
   align-items: center;
 }
 
 p {
   font-size: medium;
-  margin: 20px 0 0 0 ;
+  margin: 0;
 }
 </style>
