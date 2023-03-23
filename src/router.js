cdfb03b351df57048f495cd2e3e7bb74bc3ca83a
vtomogz <86956053+tbcompany-test@users.noreply.github.com>
2023-03-23 19:30:44 +0100

    fix web history

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import TheMainPage from './sites/TheMainPage'
 import PageNotFound from './sites/PageNotFound'
 
 export default createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: TheMainPage },
     { path: '/:any(.*)', component: PageNotFound }
