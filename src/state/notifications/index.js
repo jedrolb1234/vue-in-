57f1ba6c310a6cda3538d40dd6100022714f6ed5
andrzej <andrzej840977@gmail.com>
2023-09-01 16:47:31 +0200

    PoprawioneNotyfikacje
    Poprawka

--- src/state/notifications/index.js
+++ src/state/notifications/index.js
@@ -8,11 +8,60 @@ export default {
           description:'Link aktywacyjny został wysłany na twoją skrytkę pocztową.',
           type: 'info'
         },
+        resetpassword_email_sent: {
+          label:'Email został pomyślnie wysłany',
+          description:'Link do zmiany hasła został wygenerowany i wysłany na twoją skrzynke pocztową. ',
+          type: 'info'
+        },
+        password_changed: {
+          label:'Hasło zostało pomyślnie zmienione',
+          description:'Podczas logowania skorzystaj z nowego hasła.',
+          type: 'info'
+        },
         registration_form_invalid: {
           label:'Wypełnij formularz poprawnymi danymi.',
-          description:'Pamiętaj, że hasło musi posiadać minimum 8 znaków oraz powinno zawierać małą i dużą literę, cyfrę oraz znaku specjalny. ',
+          description:'Pamiętaj, że hasło musi posiadać minimum 8 znaków oraz powinno zawierać małą i dużą literę, cyfrę oraz znaku specjalny.',
           type: 'error'
-        }
+        },
+        common_error: {
+          label:'Coś poszło nie tak!',
+          description:'Wystąpił nieznany błąd.',
+          type: 'error'
+        },
+        login_form_invalid: {
+          label:'Wypełnij formularz poprawnymi danymi.',
+          description:'Podaj prawidłowy email oraz hasło.',
+          type: 'error'
+        },
+        user_logged: {
+          label: 'Zalogowałeś sie pomyślnie!',
+          description: 'Zosatniesz przekierowany do panelu użytkownika.',
+          type: 'info'},
+        user_name: {
+          label: 'Wprowadono niepoprawne dane!',
+          description: 'Użyto niedozwolonych znaków.',
+          type: 'info',
+        },
+        user_name_to_short: {
+          label: 'Wprowadono niepoprawne dane!',
+          description: 'Nazwa użtkownika musi zawierać conajmniej 1 literę.',
+          type: 'info',
+        },
+        game_romm_created: {
+          label: 'Pokój gry został poprawnie utowrzony!',
+          description: 'Zaproś znajomych i rozpocznij rozgrywkę.',
+          type: 'info',
+        },
+        game_room_closed: {
+          label: 'Pokój gry został poprawnie zamknięty!',
+          description: 'Zostałeś przekierowany do strony gry.',
+          type: 'info',
+        },
+        account_deleted: {
+          label:'Konto zostało skosowane.',
+          description:'Konto o podanym login nie istnieje.',
+          type: 'info'
+        },
       }
     }
   },
@@ -22,9 +71,6 @@ export default {
     },
     removeNotification(state, id) {
       state.notifications = state.notifications.filter(el => el.id !=id )
-    },
-    incrementId(state) {
-      state.id++;
     }
   },
   actions: {
@@ -33,7 +79,6 @@ export default {
         id: new Date().getTime(),
         ...notification
       };
-      context.commit('incrementId');
       context.commit('addNotification', n);
 
       setTimeout(() => {
