26c82c61625b860747f2dd6347d093f4ea6f76c3
Tomek Babiarczyk <vtomogz@gmail.com>
2023-11-11 14:27:42 +0100

    add piece marker to side panel in checkers game

--- src/components/games/SideGamePanel.vue
+++ src/components/games/SideGamePanel.vue
@@ -28,8 +28,9 @@
       </div>
       <div v-if="this.getSelectedGameRoom.status == 1">
         <h3>Rozgrywka</h3>
-        <p v-if="this.getPlayerTurn == this.getUserId" class="playerTurn" style="background-color: var(--accent);">Twój
-          ruch!</p>
+        <div style="display: flex; flex-direction: row;background-color: var(--accent); align-items: center;justify-content: center; gap:10px;" v-if="this.getPlayerTurn == this.getUserId" class="playerTurn">
+          <span :class="this.getCheckersPiece" v-if="this.getSelectedGameRoom.gameTypeId===0"></span>
+          Twój ruch!</div>
         <p v-else class="playerTurn">Ruch gracza {{ getplayerData(getPlayerTurn).userName }}</p>
         <hr>
       </div>
@@ -101,6 +102,7 @@ export default {
   },
   computed: {
     ...mapGetters(['getSelectedGameRoom', 'getUserId', 'getPlayerTurn', 'getWinner']),
+    ...mapGetters('Checkers', ['getWhitePlayer']),
     isStartGameButtonVsible() {
       if (this.getUserId == this.getSelectedGameRoom.ownerId && this.getSelectedGameRoom.players.length == this.getSelectedGameRoom.requiredNumberOfPlayers && this.getSelectedGameRoom.status == 0)
         return true;
@@ -123,12 +125,39 @@ export default {
         return 'ranking-won'
       }
       return 'ranking-lost'
+    },
+    getCheckersPiece() {
+      if(this.getUserId ===this.getWhitePlayer )
+        return 'piece-white'
+      return 'piece-black'
     }
   }
 }
 </script>
 
 <style scoped>
+.piece-white {
+  background-color: white;
+  background-image: url('data:image/svg+xml;utf8,<svg style="color: black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M576 136c0 22.09-17.91 40-40 40c-.248 0-.4551-.1266-.7031-.1305l-50.52 277.9C482 468.9 468.8 480 453.3 480H122.7c-15.46 0-28.72-11.06-31.48-26.27L40.71 175.9C40.46 175.9 40.25 176 39.1 176c-22.09 0-40-17.91-40-40S17.91 96 39.1 96s40 17.91 40 40c0 8.998-3.521 16.89-8.537 23.57l89.63 71.7c15.91 12.73 39.5 7.544 48.61-10.68l57.6-115.2C255.1 98.34 247.1 86.34 247.1 72C247.1 49.91 265.9 32 288 32s39.1 17.91 39.1 40c0 14.34-7.963 26.34-19.3 33.4l57.6 115.2c9.111 18.22 32.71 23.4 48.61 10.68l89.63-71.7C499.5 152.9 496 144.1 496 136C496 113.9 513.9 96 536 96S576 113.9 576 136z" fill="black"></path></svg>');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 60%;
+  display: block;
+  width: 30px;
+  height:30px;
+  border-radius: 50%;
+}
+.piece-black {
+  background-color: black;
+  background-image: url('data:image/svg+xml;utf8,<svg style="color: white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M576 136c0 22.09-17.91 40-40 40c-.248 0-.4551-.1266-.7031-.1305l-50.52 277.9C482 468.9 468.8 480 453.3 480H122.7c-15.46 0-28.72-11.06-31.48-26.27L40.71 175.9C40.46 175.9 40.25 176 39.1 176c-22.09 0-40-17.91-40-40S17.91 96 39.1 96s40 17.91 40 40c0 8.998-3.521 16.89-8.537 23.57l89.63 71.7c15.91 12.73 39.5 7.544 48.61-10.68l57.6-115.2C255.1 98.34 247.1 86.34 247.1 72C247.1 49.91 265.9 32 288 32s39.1 17.91 39.1 40c0 14.34-7.963 26.34-19.3 33.4l57.6 115.2c9.111 18.22 32.71 23.4 48.61 10.68l89.63-71.7C499.5 152.9 496 144.1 496 136C496 113.9 513.9 96 536 96S576 113.9 576 136z" fill="white"></path></svg>');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 60%;
+  display: block;
+  width: 30px;
+  height:30px;
+  border-radius: 50%;
+}
 .ranking-won {
   color: var(--accent);
 }
