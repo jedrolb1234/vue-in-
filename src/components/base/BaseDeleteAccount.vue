3f67b3a1e7e5f8638b3e70acc7815735734b569e
Tomek Babiarczyk <vtomogz@gmail.com>
2023-11-15 21:01:36 +0100

    delete fix

--- src/components/base/BaseDeleteAccount.vue
+++ src/components/base/BaseDeleteAccount.vue
@@ -32,7 +32,7 @@ import { mapActions } from 'vuex';
     },
     deleteA(){
       this.$store.dispatch('deleteAccount');
-      this.$store.dispatch('logOutUser');
+      // this.$store.dispatch('logOutUser');
     }
     }
   }
